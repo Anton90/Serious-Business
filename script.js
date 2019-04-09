ddd07441e19a3fcafd1e818062b9e1019cf82f23
Anton Everaert <antoneveraert@gmail.com>
2019-04-09 09:42:04 +0200

    added getDayFunction to js file

---
+++ script.js
@@ -0,0 +1,19 @@
+var button = document.getElementsByClassName("btn")[0];
+button.addEventListener("click", getDayFunction);
+
+
+
+function getDayFunction() {
+  var d = new Date();
+  var weekday = new Array(7);
+  weekday[0] = "Sunday";
+  weekday[1] = "Monday";
+  weekday[2] = "Tuesday";
+  weekday[3] = "Wednesday";
+  weekday[4] = "Thursday";
+  weekday[5] = "Friday";
+  weekday[6] = "Saturday";
+
+  var n = weekday[d.getDay()];
+  console.log(n);
+}
