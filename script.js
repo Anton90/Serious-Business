fd33da228aff9bae7c74cbed35d8c5958dc83f5c
Anton Everaert <antoneveraert@gmail.com>
2019-04-09 11:34:17 +0200

    added working if statement

--- script.js
+++ script.js
@@ -9,23 +9,21 @@ const weekday = new Array(7);
   weekday[6] = "Saturday";
 
 const currentDay = weekday[today.getDay()];
-const time = today.getHours() + ":" + today.getMinutes();
+const currentHour = today.getHours();
+const currentMinute = today.getMinutes();
 var morning1 = document.getElementsByClassName("hoursMorning")[today.getDay() - 1];
 var morning2 = document.getElementsByClassName("minutesMorning")[today.getDay() - 1];
 var evening1 = document.getElementsByClassName("hoursEvening")[today.getDay() - 1];
 var evening2 = document.getElementsByClassName("minutesEvening")[today.getDay() - 1];
-var morningHour = parseInt(morning1);
-var morningMinutes = parseInt(morning2);
-var eveningHour = parseInt(evening1);
-var eveningMinutes = parseInt(evening2);
-
-console.log(time);
-console.log(morningHour);
-console.log(eveningHour);
+var morningHour = parseInt(morning1.innerHTML);
+var morningMinutes = parseInt(morning2.innerHTML);
+var eveningHour = parseInt(evening1.innerHTML);
+var eveningMinutes = parseInt(evening2.innerHTML); 
 
 if (currentDay != "Sunday") {
-  
+  if (morningHour <= currentHour < eveningHour) {
     console.log("We're open");
+  }
 } else {
   console.log("We're closed");
 }
