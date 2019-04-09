f8d0e79ecc1d0bfeb118734328077301d3c6fbd0
Anton Everaert <antoneveraert@gmail.com>
2019-04-09 11:19:15 +0200

    changed span class names in html

--- script.js
+++ script.js
@@ -10,6 +10,23 @@ const weekday = new Array(7);
 
 const currentDay = weekday[today.getDay()];
 const time = today.getHours() + ":" + today.getMinutes();
+var morning1 = document.getElementsByClassName("hoursMorning")[today.getDay() - 1];
+var morning2 = document.getElementsByClassName("minutesMorning")[today.getDay() - 1];
+var evening1 = document.getElementsByClassName("hoursEvening")[today.getDay() - 1];
+var evening2 = document.getElementsByClassName("minutesEvening")[today.getDay() - 1];
+var morningHour = parseInt(morning1);
+var morningMinutes = parseInt(morning2);
+var eveningHour = parseInt(evening1);
+var eveningMinutes = parseInt(evening2);
 
-if ()
+console.log(time);
+console.log(morningHour);
+console.log(eveningHour);
+
+if (currentDay != "Sunday") {
+  
+    console.log("We're open");
+} else {
+  console.log("We're closed");
+}
 
